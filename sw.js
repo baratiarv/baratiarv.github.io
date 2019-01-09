b147ed1ac2c528de7c4397505c0f3f6c5cbdac9d
Bharathi <baratiarv@gmail.com>
2019-01-10 00:29:57 +0530

    :zap: PWA changes

---
+++ sw.js
@@ -0,0 +1,31 @@
+const version = "1.0.0";
+const cacheName = `radios-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/hindi.html`,
+        `/telugu.html`,
+        `/js/mep-feature-playlist.js`,
+        `/styles/styles.css`,
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
